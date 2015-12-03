1c4d7e11c880a6bf54e503f369b37d5429f03bc4
Sean M. Snyder <sean.snyder@snhu.edu>
2015-12-03 08:30:04 -0500

    changes for printing/ added jquery for selected compliment

--- app/assets/javascripts/compliments.js
+++ app/assets/javascripts/compliments.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+	$(".compliment").each(function(index){
+	  if(index % 3 === 0 && index !== 0) {
+	    $(this).addClass("page-break")
+	    console.log(index);
+	  }
+	})
 
+	$(".compliment").hover(function() {
+		$(this).addClass("compliment-selected");
+	},
+	function() {
+
+		$(this).removeClass("compliment-selected");
+	})
 });
