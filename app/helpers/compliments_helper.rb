e83bf0fcb5d0494cee0a1993384ac4457f97c222
Sean M. Snyder <sean.snyder@snhu.edu>
2015-12-14 12:24:13 -0500

    fix regex

--- app/helpers/compliments_helper.rb
+++ app/helpers/compliments_helper.rb
@@ -1,7 +1,7 @@
 module ComplimentsHelper
   def parseCompliments(data)
-    data.scan(/([0-9]+[\/][0-9]+[\/][0-9]{4}|[0-9]+[\/][0-9]+[\/][0-9]{2})[\t ]+([0-9]+)[\t ]+([a-zA-Z ]+)/) do
-      if $3.present?
+      data.scan(/([0-9]+[\/][0-9]+[\/][0-9]{4}|[0-9]+[\/][0-9]+[\/][0-9]{2})[\t ]+([0-9]+)[\t ]+([a-zA-Z ,]+)/) do
+        if $3.present?
         if $3.include?(", ")
           driver_name = $3.split(", ").reverse.join(" ")
         elsif $3.include?(",")
